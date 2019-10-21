1dfe07b8f7f2dced7f3efc6eb28e14bd6937d1ae
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-21 23:08:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,8 +36,6 @@ def winner(board)
     return "X"
   elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) =="O"
     return "O"
-  else
-    return nil
-  end
-end
+      end
+    end
 end
