2636b5fe314ecb540328c027d2cb9012fadc9a39
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-21 23:06:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,3 +29,15 @@ end
 def over?(board)
   won?(board) || draw?(board)
 end
+
+def winner?(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+  if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) =="X"
+    return "X"
+  elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) =="O"
+    return "O"
+  else
+    return nil
+  end
+end
+end
